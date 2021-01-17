9280ad7ad800e6326452f78464c9039f2afd91b0
Giuseppe Cerbero <Giuseppe.Cerbero@alliance-healthcare.net>
2021-01-17 23:38:45 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Click me
+    </Button>
+      <h1>Hello gant react app</h1>
+
+    </div>
+  );
+}
+
+export default App;
