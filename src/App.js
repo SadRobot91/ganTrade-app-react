46149a120945ce9b69e6304945418cad7dae7b51
Yuliya Pelekh <giuliapelekh@gmail.com>
2021-01-18 23:29:08 +0100

    dashboard - appBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import { Button } from '@material-ui/core';
+import AppBarDashboard from './components/AppBarDashboard';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="primary">
-        Click me
-    </Button>
-      <h1>Hello gant react app</h1>
-
+       <AppBarDashboard />
     </div>
   );
 }
